b4cf082cff4b22af3ee8a05709993e63b3138749
Eliiiise <balvalloire@gmail.com>
2020-03-22 22:57:36 +0100

    project transition (image + text) done

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -1,6 +1,6 @@
 let cpt = 0;
 let ecouteurScroll = 1;
-const tempsTransition = 1000;
+const tempsTransition = 900;
 
 export default {
   init () {
@@ -14,60 +14,109 @@ export default {
       titles: document.querySelectorAll('.js-title-project'),
       images: document.querySelectorAll('.js-image-presentation-projet'),
       divs: document.querySelectorAll('.js-projet'),
+      div: document.querySelector('.js-projets:nth-of-type(1)'),
       projects: document.querySelector('.projets'),
       scroll: window.scrollY,
     };
   },
 
   initEvents () {
+    window.scrollTo(0,0);
+    this.$els.divs[0].scrollTo( 0 , this.$els.divs[0].clientHeight/20 );
     this.getWichProject();
+    this.baseMonte();
+    this.getTitle();
+    this.getImage();
   },
 
   getWichProject() {
     this.$els.divs.forEach((div) => {
-
-      if ( ecouteurScroll == 0 ) {
-        div.scrollTo(0,div.clientHeight/20);
-        console.log('centrage');
-      }
-
-      else {
         div.addEventListener('scroll', () => {
-
-          if ( div.scrollTop >= div.clientHeight/11) {
+          console.log(window.scrollY);
+          if ( div.scrollTop >= div.clientHeight/11 && ecouteurScroll==1) {
             console.log('next');
             cpt++;
+            if (cpt == 5) {
+              cpt = 0;
+            }
             ecouteurScroll = 0;
-            this.change(cpt);
+            this.baseDescend(1);
+            this.change(cpt,+1);
           }
-          else if ( div.scrollTop == 0) {
+          else if ( div.scrollTop == 0 && ecouteurScroll==1) {
             console.log('previous');
             cpt--;
+            if (cpt == -1) {
+              cpt = 4;
+            }
             ecouteurScroll = 0;
-            this.change(cpt);
+            this.baseMonte(1);
+            this.change(cpt,-1);
           }
         }, false);
-      }
-
     });
   },
 
-  change(cpt) {
-    this.$els.projects.style.transform = `translateY(${-100*cpt}vh)`;
+  change(cpt,s) {
     setTimeout(() => {
-      ecouteurScroll = 1;
+      this.$els.projects.style.transform = `translateY(${-100*cpt}vh)`;
+      window.scrollTo(0,0);
+      if (s<0) {
+        this.baseDescend(0);
+      }
+      else {
+        this.baseMonte(0);
+      }
+    }, tempsTransition/2);
+
+    setTimeout(() => {
+        this.$els.divs.forEach((div) => {
+          div.scrollTo(0,div.clientHeight/20);
+          this.getTitle();
+          this.getImage();
+          ecouteurScroll = 1;
+        });
     }, tempsTransition);
   },
 
   getTitle() {
     this.$els.titles.forEach(function (title) {
-      title.style.transform = 'translateY(300px)';
       setTimeout( () => {
-        title.style.transition = 'all 2s';
-        title.style.transform = 'translateY(100px)';
-        this.getWichProject();
+        title.style.transition = 'all 1s';
+        title.style.transform = 'translateY(0px)';
       }, 100);
     });
+  },
+
+  getImage() {
+    this.$els.images.forEach(function (image) {
+      setTimeout( () => {
+        image.style.transition = 'all 1s';
+        image.style.transform = 'translateY(0px) rotate(10deg)';
+      }, 100);
+    });
+  },
+
+  baseMonte(t) {
+    this.$els.titles.forEach(function (title) {
+    title.style.transform = 'translateY(300px)';
+      title.style.transition = `all ${t}s`;
+    });
+    this.$els.images.forEach(function (image) {
+    image.style.transform = 'translate(-100px,1000px) rotate(50deg)';
+    image.style.transition = `all ${t}s`;
+    });
+  },
+
+  baseDescend(t) {
+      this.$els.titles.forEach(function (title) {
+        title.style.transform = 'translateY(-300px)';
+        title.style.transition = `all ${t}s`;
+      });
+      this.$els.images.forEach(function (image) {
+        image.style.transform = 'translate(-100px,-1000px) rotate(-40deg)';
+        image.style.transition = `all ${t}s`;
+      });
   },
 
   finalize() {
--- resources/views/page-projets.blade.php
+++ resources/views/page-projets.blade.php
@@ -15,20 +15,20 @@
           $nbProjet++;
         @endphp
           <div class="content-projects">
-           <div class="projet js-projet">
-            <div class="scroll">
-              <div class="content-project">
-                  <a href="{{ $project->link }}">
+           <a href="{{ $project->link }}">
+             <div class="projet js-projet">
+              <div class="scroll">
+                <div class="content-project">
                     <img src="{{ $project->thumb }}" alt="{{ $project->alt }}" title="{{ $project->title }}" class="image-presentation-projet js-image-presentation-projet">
-                  </a>
-                  <span class="js-span-title-project">
-                    <h1 class="js-title-project js-hidden no-hidden">{{ $project->name }}</h1>
-                  </span>
-                  <p class="miniInfo js-miniInfo">{{ $project->type }} - {{ $project->date }}<br>Projet 0{{ $nbProjet }}</p>
-                  <p class="nbProjet js-npProjet">0{{ $nbProjet }}</p>
+                    <span class="js-span-title-project">
+                      <h1 class="js-title-project js-hidden no-hidden">{{ $project->name }}</h1>
+                    </span>
+                    <p class="miniInfo js-miniInfo">{{ $project->type }} - {{ $project->date }}<br>Projet 0{{ $nbProjet }}</p>
+                    <p class="nbProjet js-npProjet">0{{ $nbProjet }}</p>
+                  </div>
                 </div>
-              </div>
-           </div>
+             </div>
+            </a>
           </div>
 
 
